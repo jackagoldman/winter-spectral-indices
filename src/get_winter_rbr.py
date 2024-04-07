6cc66d134fe1be98877f4d04bca0c150593101f6
jackagoldman <jandrewgoldman@gmail.com>
2024-04-07 11:29:40 -0400

    update winter rbr function

--- src/get_winter_rbr.py
+++ src/get_winter_rbr.py
@@ -3,16 +3,34 @@ def getWinterRBR(fires, img_coll):
    fids = fires.aggregate_array('Fire_ID')
    # turn it to python list
    fids = fids.getInfo()
-   # get length of ID list
-   idSize = fids.size()
-   idSize = fids.getInfo()
+   
+   res = ee.ImageCollection(fires.map(indices_function))
   
   # loop through list of fire ids
-  for i in fids:
+  for j in fids:
+  name = j
+  # get fire that corresponds to i
+  foi = res.filter(ee.Filter.eq('Fire_ID', name))
   
+  b = fires.filter(ee.Filter.eq('Fire_ID', name))
   
+  fB = b.geometry().bounds()
   
+  fiya = ee.Image(foi.filterMetadata('fireID', 'equals', name).first());
+  #get image
   
+  task = ee.batch.Export.image.toDrive(**{
+    'image': fiya,
+    'description': name,
+    'folder':'winter_test_folder',
+    'scale': 30,
+    'region': fB.getInfo()['coordinates']
+    })
+  # start task
+  task.start()
+ 
 
-  
-  
+task.status()
+
+
+return
